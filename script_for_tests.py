4818f198119dbf0a149fdff5a3b596e15f3bb4c1
LachezarStoev <lachezarstoev93@gmail.com>
2014-03-23 19:33:36 +0200

    adding script again

--- script_for_tests.py
+++ script_for_tests.py
@@ -4,7 +4,7 @@ from subprocess import call
 def main():
 	dirs=[dir for dir in os.listdir() if dir.find(".") == -1 and dir != "MEGATRON" and dir != "generate_numbers_test"]
 	for dir in dirs:
-		call("py " + dir + "/tests.py",shell=True)
+		call("python " + dir + "/tests.py",shell=True)
 
 if __name__ == '__main__':
 	main()
